fbaa6c34037e845f36fc910de439a3cfb6e4a317
manimaran85 <maranjoy@gmail.com>
2023-01-07 17:13:47 -0600

    HelloWorld commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,41 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=2;
+		int y=5;
+		int z=10;
+	
+		String firstName="Manimaran";
+		String lastName="Muthuswamy";
+		Integer age=x+y;
+		Boolean weatherIsHot= true;
+	
+		int a=(x+y)*z;
+		int b=a-(a*y);
+	// b = a-b;
+		int num1=3;
+		int num2=3;
+		int num3=-10;
+		
+		int n2=num1+num2*num3;
+		System.out.println(n2);
+		System.out.println(9.0/30.0);
+		
+		
+	/*	double n1=(15/7)*2;
+		
+		int ans=(num1*num2)%num3;
+		System.out.println(ans);
+		
+				System.out.println(a);
+				System.out.println(b);
+				
+	System.out.println(firstName);
+	System.out.println(lastName);
+	System.out.println(age);
+	System.out.println(weatherIsHot);
+	*/
+	}
+
+}
